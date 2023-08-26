e36a8efcc82aadb158cd88ff3c6eed63afea39f3
Yuri Silva <yrgamerbr@gmail.com>
2023-08-26 12:33:07 -0300

    3d changes

--- src/components/homepage/world.tsx
+++ src/components/homepage/world.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { MeshTransmissionMaterial, useFBO, useGLTF } from "@react-three/drei";
 import {
   Canvas,
   MaterialNode,
+  createPortal,
   extend,
   useFrame,
   useLoader,
 } from "@react-three/fiber";
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
+import * as THREE from "three";
 import { DoubleSide, TextureLoader } from "three";
 
 import PlaneMaterial from "@/components/homepage/shader/planeMaterial";
+import { useTheme } from "next-themes";
+import { GLTF } from "three-stdlib";
 
 extend({ PlaneMaterial });
 
@@ -27,8 +32,8 @@ const World: FC<WorldProps> = ({}) => {
   return (
     <Canvas
       camera={{
-        fov: 10,
-        position: [2, -2, 5],
+        fov: 17,
+        position: [0, 0, 4.2],
       }}
     >
       <Experience />
@@ -50,10 +55,65 @@ const Experience: FC<ExperienceProps> = ({}) => {
   }, [image]);
 
   return (
-    <mesh>
-      <planeGeometry args={[0.4, 0.6, 16, 16]} />
-      <planeMaterial ref={planeMat} side={DoubleSide} />
-    </mesh>
+    <Lens>
+      <mesh position={[0, 0, -9]} rotation={[-0.4, -0.5, 0]}>
+        <planeGeometry args={[1, 1.4, 16, 16]} />
+        <planeMaterial ref={planeMat} side={DoubleSide} />
+      </mesh>
+    </Lens>
+  );
+};
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Cylinder: THREE.Mesh;
+  };
+};
+
+interface LensProps {
+  children: any;
+}
+
+const Lens: FC<LensProps> = ({ children }) => {
+  const { nodes } = useGLTF("/lens-transformed.glb") as GLTFResult;
+  const buffer = useFBO();
+  const [scene] = useState(() => new THREE.Scene());
+  const { theme } = useTheme();
+
+  const color = {
+    dark: new THREE.Color("#44403C"),
+    light: new THREE.Color("#D8D7D7"),
+  };
+
+  const bgColor = useRef<THREE.Color>(color.dark);
+
+  if (theme) bgColor.current = color[theme as keyof typeof color];
+
+  useFrame((state) => {
+    state.gl.setRenderTarget(buffer);
+    state.gl.setClearColor(bgColor.current);
+    state.gl.render(scene, state.camera);
+    state.gl.setRenderTarget(null);
+  });
+
+  return (
+    <>
+      {createPortal(children, scene)}
+      <mesh
+        scale={[2, 2, 1]}
+        rotation={[Math.PI / 2, 0, 0]}
+        geometry={nodes.Cylinder.geometry}
+      >
+        {/* @ts-expect-error idk */}
+        <MeshTransmissionMaterial
+          buffer={buffer.texture}
+          thickness={2}
+          anisotropy={0.2}
+          chromaticAberration={0.02}
+          ior={4}
+        />
+      </mesh>
+    </>
   );
 };
 
--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,47 +1,25 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { inter } from "@/font/font";
-import { cn } from "@/lib/utils";
 import { HalfMoon, SunLight } from "iconoir-react";
 import { useTheme } from "next-themes";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 
 interface ThemeSwitchProps {}
 
 const ThemeSwitch: FC<ThemeSwitchProps> = ({}) => {
   const { theme, setTheme } = useTheme();
-  const [position, setPosition] = useState<string>(theme!);
-
-  useEffect(() => setTheme(position), [position, setTheme]);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className={inter.className}>
-        <Button variant="ghost" className="h-full">
-          <SunLight className="rotate-0 scale-100 transition-all dark:-rotate-180 dark:scale-0" />
-          <HalfMoon className="absolute rotate-180 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className={cn(inter.className, "w-56")}>
-        <DropdownMenuLabel>Themes</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-          <DropdownMenuRadioItem value="light">Light</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="dark">Dark</DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="ghost"
+      className="h-full"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <SunLight className="rotate-0 scale-100 transition-all dark:-rotate-180 dark:scale-0" />
+      <HalfMoon className="absolute rotate-180 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 };
 
